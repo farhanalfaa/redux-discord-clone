659588c5b41b41c98d161727e46acd3ed414b36f
Farhan Alfariqi <falfariqi18@gmail.com>
2020-10-20 20:22:18 +0700

    Updating Multi Channel Session

--- src/Message.js
+++ src/Message.js
@@ -2,16 +2,18 @@ import React from "react";
 import "./Message.css";
 import { Avatar } from "@material-ui/core";
 
-function Message() {
+function Message({ timestamp, user, message }) {
   return (
     <div className="message">
-      <Avatar />
+      <Avatar src={user.photo} />
       <div className="message__info">
         <h4>
-          farhanalfaa
-          <span className="message__timestamp">this is a timestamp</span>
+          {user.displayName}
+          <span className="message__timestamp">
+            {new Date(timestamp?.toDate()).toUTCString()}
+          </span>
         </h4>
-        <p>This is a message!</p>
+        <p>{message}</p>
       </div>
     </div>
   );
